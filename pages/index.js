9e581f8ab851edec240a9d49bb10b82e7ccf352c
erkinun <erkinun@gmail.com>
2023-11-01 16:29:24 +0000

    a simple duty prediction

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,39 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
+function findtheDate(dateString = "06 November", year = 2023) {
+  const date = new Date(`${dateString} ${year}`);
+  return date;
+}
+
+// todo how to sort by date?
+function dateSorter(a, b) {
+  return a - b;
+}
+
+function whichFlatIsNext(knownLastDuty, nextCollection) {
+  const flats = ["A", "B", "C"];
+
+  const lastDutyIndex = flats.indexOf(knownLastDuty.flat);
+  const knownLastDutyDate = new Date(knownLastDuty.date);
+
+  // difference between next collection date and last known date
+  const diff = nextCollection - knownLastDutyDate;
+  // how many weeks is that?
+  const weeks = diff / (1000 * 60 * 60 * 24 * 7);
+  const nextDutyIndex = (lastDutyIndex + weeks) % flats.length;
+  return flats[nextDutyIndex];
+}
+
 export default function Home(props) {
-  console.log(props.Refuse_collection_dates);
+  const knownLastDuty = { flat: "B", date: "2023-10-30" };
+  const today = new Date();
+
+  const nextCollection = props.Refuse_collection_dates.map(
+    (item) => item.Next_Collection
+  )
+    .map((d) => findtheDate(d, today.getFullYear()))
+    .sort(dateSorter)[0]; // bit hacky
 
   return (
     <div className={styles.container}>
@@ -12,11 +43,23 @@ export default function Home(props) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
+      <main
+        className={styles.main}
+        style={{ display: "flex", flexDirection: "column", gap: "1rem" }}
+      >
         <h1 className={styles.title}>Bin collection days</h1>
         <h2>For 36 Muswell Avenue</h2>
 
         <section>Today is {new Date().toDateString()}</section>
+        <section>
+          Based on the information that flat {knownLastDuty.flat} has done the
+          bins on {knownLastDuty.date}{" "}
+        </section>
+        <section>
+          Therefore the next duty is for flat{" "}
+          {whichFlatIsNext(knownLastDuty, nextCollection)} on{" "}
+          {nextCollection.toDateString()}
+        </section>
 
         <ul>
           {props.Refuse_collection_dates.map((item) => {
@@ -47,7 +90,7 @@ export default function Home(props) {
   );
 }
 
-export async function getServerSideProps(context) {
+export async function getServerSideProps() {
   const res = await fetch(
     "https://my.haringey.gov.uk/getdata.aspx?RequestType=LocalInfo&ms=mapsources/MyHouse&format=JSON&group=Property%20details|Refuse%20collection%20dates&uid=100021204359"
   );
